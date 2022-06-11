d1dd6cd33015ea0cea3f363e0ae6c71be4138eca
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2022-06-11 18:09:56 -0300

    Test eventhandler for number buttons

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-function add(a,b) {
-    alert(a + b);
-}
+// function add(a,b) {
+//     alert(a + b);
+// }
 
-function subtract(a,b) {
-    alert(a - b);
-}
+// function subtract(a,b) {
+//     alert(a - b);
+// }
 
-function multiply(a,b) {
-    alert(a * b);
-}
+// function multiply(a,b) {
+//     alert(a * b);
+// }
 
-function divide(a,b) {
-    alert(a / b);
-}
+// function divide(a,b) {
+//     alert(a / b);
+// }
 
-function operate() {
-    let a = parseInt(prompt("First number: "));
-    let b = parseInt(prompt("Second number: "));
-    let operation = prompt("Add, subtract, multiply or divide?").toLowerCase();
+// function operate() {
+//     let a = parseInt(prompt("First number: "));
+//     let b = parseInt(prompt("Second number: "));
+//     let operation = prompt("Add, subtract, multiply or divide?").toLowerCase();
 
-    switch(operation) {
-        case 'add':
-            add(a,b)
-            break;
-        case 'subtract':
-            subtract(a,b);
-            break;
-        case 'multiply':
-            multiply(a,b);
-            break;
-        case 'divide':
-            divide(a,b);
-    }
-}
+//     switch(operation) {
+//         case 'add':
+//             add(a,b)
+//             break;
+//         case 'subtract':
+//             subtract(a,b);
+//             break;
+//         case 'multiply':
+//             multiply(a,b);
+//             break;
+//         case 'divide':
+//             divide(a,b);
+//     }
+// }
 
-operate();
+// for(let i = 0; i < 9; i++) {
+//     const buttonValue = document.getElementById(i);
+//     console.log(buttonValue);
+// }
+
+
+let numberButtons = document.querySelectorAll('.number');
+    numberButtons.forEach(button => {
+        button.addEventListener('click', function(e) {
+            console.log(e.target.innerText);
+        });
+    });
