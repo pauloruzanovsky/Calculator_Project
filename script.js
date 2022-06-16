9975444baa4f3c4f4456392a145a9f8aaaea6ae0
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2022-06-15 22:21:31 -0300

    Update CSS design, attempt to add keyboard support

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ displayContainer.appendChild(display);
 let value1 = '';
 let value2 = '';
 let currentOperation = '';
+let runningResult;
 
 
 //This function sends the numbers to the display once 
@@ -28,45 +29,133 @@ numberButtons.forEach(button => {
 });
 }
 
+function returnKeyboardNumberInput() {
+    document.addEventListener('keydown', (event) => {
+            if(isNaN(+event.key)=== false)
+            return display.textContent += event.key;
+
+            if(event.key === '.' && display.textContent.indexOf('.') !== -1){
+                return display.textContent;
+            }
+    
+            if(value1 !== '' && value1 == display.textContent) {
+                display.textContent = '';
+            }
+        });
+    }
+
+function returnKeyboardCurrentOperation() {
+    document.addEventListener('keydown', (event) => {
+            console.log(event.key);
+            if(event.key === 'Backspace') {
+            return display.textContent = display.textContent.slice(0,-1)
+            }
+            
+            if(event.key === '=' && display.textContent === '') {
+                return;
+            } 
+            if(event.key !== '+' || event.key !== '-' ||event.key !== '*' || event.key !== '/' ) {
+                return
+            }
+            
+            if (value1 === '') {
+                value1 = +display.textContent;
+                if(event.key === '+'){
+                    return currentOperation = 'add';
+                }
+                if(event.key === '-'){
+                    return currentOperation = 'subtract';
+                }
+                if(event.key === '*'){
+                    return currentOperation = 'multiply';
+                }
+                if(event.key === '/'){
+                    return currentOperation = 'divide';
+                }
+                
+            }   
+
+            value2 = +display.textContent;
+            if(currentOperation === 'add') {
+                runningResult = value1 + value2;
+            } else if(currentOperation === 'subtract') {
+                runningResult = value1 - value2;
+            } else if(currentOperation === 'multiply') {
+                runningResult = value1 * value2;
+            } else if(currentOperation === 'divide') {
+                runningResult = value1/value2;
+            }
+
+            value1 = runningResult;
+            value2 = '';
+            display.textContent = Math.round((runningResult + Number.EPSILON)*100)/100;
+
+            if(event.key === '+'){
+                currentOperation = 'add';
+            }
+            if(event.key === '-'){
+                currentOperation = 'subtract';
+            }
+            if(event.key === '*'){
+                currentOperation = 'multiply';
+            }
+            if(event.key === '/'){
+                currentOperation = 'divide';
+            }
+
+    });
+}
+
+
 function operate() {
-    let runningResult;
-    returnNumberFromButton();
+
+returnNumberFromButton();
     let operationButtons = document.querySelectorAll('.operation');
     operationButtons.forEach(button => {
         button.addEventListener('click', function(e) {
+
+            if(e.target.classList[1] === 'delete') {
+                return display.textContent = display.textContent.slice(0,-1)
+            }
+
             if(e.target.classList[1] === 'clear'){
                 display.textContent = '';
                 value1 = '';
                 value2 = '';
                 runningResult = '';
-            } else if(e.target.classList[1] === 'equal' && display.textContent === '') {
-            } else {
-            if (value1 === '') {
-                value1 = +display.textContent;
-                currentOperation = e.target.classList[1];
+                return;
+            } 
+            
+            if(e.target.classList[1] === 'equal' && display.textContent === '') {
+                return;
             } 
 
-            else {
-                value2 = +display.textContent;
-                if(currentOperation === 'add') {
-                    runningResult = value1 + value2;
-                } else if(currentOperation === 'subtract') {
-                    runningResult = value1 - value2;
-                } else if(currentOperation === 'multiply') {
-                    runningResult = value1 * value2;
-                } else if(currentOperation === 'divide') {
+            if (value1 === '') {
+                value1 = +display.textContent;
+                return currentOperation = e.target.classList[1];
+            }   
 
-                    runningResult = value1/value2;
-                }
-                currentOperation = e.target.classList[1];
-                value1 = runningResult;
-                value2 = '';
-                display.textContent = Math.round((runningResult + Number.EPSILON)*100)/100;
+            value2 = +display.textContent;
+            if(currentOperation === 'add') {
+                runningResult = value1 + value2;
+            } else if(currentOperation === 'subtract') {
+                runningResult = value1 - value2;
+            } else if(currentOperation === 'multiply') {
+                runningResult = value1 * value2;
+            } else if(currentOperation === 'divide') {
+
+                runningResult = value1/value2;
             }
+            currentOperation = e.target.classList[1];
+            value1 = runningResult;
+            value2 = '';
+            display.textContent = Math.round((runningResult + Number.EPSILON)*100)/100;
             }
+        )
             
 });
-});
-}
+};
 
 operate();
+returnKeyboardCurrentOperation();
+returnKeyboardNumberInput();
