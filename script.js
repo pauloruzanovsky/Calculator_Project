37ff0be88cbd652e1a1acfa9a940472d1fe0dd33
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2022-06-16 09:42:39 -0300

    Finish calculator project

--- script.js
+++ script.js
@@ -1,161 +1,137 @@
-let display = document.createElement('div');
-display.classList.add('display');
-const displayContainer = document.querySelector('.displayContainer');
-displayContainer.appendChild(display);
-let value1 = '';
-let value2 = '';
-let currentOperation = '';
-let runningResult;
+const numberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operation')
+const equalsButton = document.querySelector('.equal')
+const clearButton = document.querySelector('.clear')
+const deleteButton = document.querySelector('.delete')
+const pointButton = document.querySelector('.point')
+const lastOperationScreen = document.getElementById('lastOperationDisplay')
+const currentOperationScreen = document.getElementById('currentOperationDisplay')
 
+let value1 = ''
+let value2 = ''
+let currentOperation = null
+let shouldResetScreen = false
 
-//This function sends the numbers to the display once 
-//the buttons are clicked
-function returnNumberFromButton() {
-let numberButtons = document.querySelectorAll('.number');
-numberButtons.forEach(button => { 
-    button.addEventListener('click', function(e) {
-        if(e.target.innerText === '.' && display.textContent.indexOf('.') !== -1){
-            return display.textContent;
-        }
+window.addEventListener('keydown', handleKeyboardInput)
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+pointButton.addEventListener('click', appendPoint)
 
-        if(value1 !== '' && value1 == display.textContent) {
-            display.textContent = '';
-        }
-        
-        display.textContent += e.target.innerText;
-        return display.textContent;
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
 
-    });
-});
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => console.log(setOperation(button.textContent))
+))
+
+function appendNumber(number) {
+    if (currentOperationScreen.textContent === '0' || shouldResetScreen)
+      resetScreen()
+    currentOperationScreen.textContent += number
 }
 
-function returnKeyboardNumberInput() {
-    document.addEventListener('keydown', (event) => {
-            if(isNaN(+event.key)=== false)
-            return display.textContent += event.key;
+function resetScreen() {
+    currentOperationScreen.textContent = ''
+    shouldResetScreen = false
+}
 
-            if(event.key === '.' && display.textContent.indexOf('.') !== -1){
-                return display.textContent;
-            }
-    
-            if(value1 !== '' && value1 == display.textContent) {
-                display.textContent = '';
-            }
-        });
+function clear() {
+    currentOperationScreen.textContent = '0'
+    lastOperationScreen.textContent = ''
+    value1 = ''
+    value2 = ''
+    currentOperation = null
+}
+
+function appendPoint() {
+    if (shouldResetScreen) resetScreen()
+    if (currentOperationScreen.textContent === '')
+        currentOperationScreen.textContent = '0'
+    if (currentOperationScreen.textContent.includes('.')) return
+    currentOperationScreen.textContent += '.'
+}
+  
+function deleteNumber() {
+    currentOperationScreen.textContent = currentOperationScreen.textContent
+      .toString()
+      .slice(0, -1)
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    value1 = currentOperationScreen.textContent
+    currentOperation = operator;
+    lastOperationScreen.textContent = `${value1} ${currentOperation}`
+    shouldResetScreen = true
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+      alert("You can't divide by 0!")
+      return
     }
+    value2 = currentOperationScreen.textContent
+    currentOperationScreen.textContent = roundResult(
+      operate(currentOperation, value1, value2)
+    )
+    lastOperationScreen.textContent = `${value1} ${currentOperation} ${value2} =`
+    currentOperation = null
+  }
+  
+  function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+  }
+  
+  function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if (e.key === '.') appendPoint()
+    if (e.key === '=' || e.key === 'Enter') evaluate()
+    if (e.key === 'Backspace') deleteNumber()
+    if (e.key === 'Escape') clear()
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+      setOperation(convertOperator(e.key))
+  }
+  
+  function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷'
+    if (keyboardOperator === '*') return '×'
+    if (keyboardOperator === '-') return '−'
+    if (keyboardOperator === '+') return '+'
+  }
 
-function returnKeyboardCurrentOperation() {
-    document.addEventListener('keydown', (event) => {
-            console.log(event.key);
-            if(event.key === 'Backspace') {
-            return display.textContent = display.textContent.slice(0,-1)
-            }
-            
-            if(event.key === '=' && display.textContent === '') {
-                return;
-            } 
-            if(event.key !== '+' || event.key !== '-' ||event.key !== '*' || event.key !== '/' ) {
-                return
-            }
-            
-            if (value1 === '') {
-                value1 = +display.textContent;
-                if(event.key === '+'){
-                    return currentOperation = 'add';
-                }
-                if(event.key === '-'){
-                    return currentOperation = 'subtract';
-                }
-                if(event.key === '*'){
-                    return currentOperation = 'multiply';
-                }
-                if(event.key === '/'){
-                    return currentOperation = 'divide';
-                }
-                
-            }   
-
-            value2 = +display.textContent;
-            if(currentOperation === 'add') {
-                runningResult = value1 + value2;
-            } else if(currentOperation === 'subtract') {
-                runningResult = value1 - value2;
-            } else if(currentOperation === 'multiply') {
-                runningResult = value1 * value2;
-            } else if(currentOperation === 'divide') {
-                runningResult = value1/value2;
-            }
-
-            value1 = runningResult;
-            value2 = '';
-            display.textContent = Math.round((runningResult + Number.EPSILON)*100)/100;
-
-            if(event.key === '+'){
-                currentOperation = 'add';
-            }
-            if(event.key === '-'){
-                currentOperation = 'subtract';
-            }
-            if(event.key === '*'){
-                currentOperation = 'multiply';
-            }
-            if(event.key === '/'){
-                currentOperation = 'divide';
-            }
-
-    });
-}
-
-
-function operate() {
-
-returnNumberFromButton();
-    let operationButtons = document.querySelectorAll('.operation');
-    operationButtons.forEach(button => {
-        button.addEventListener('click', function(e) {
-
-            if(e.target.classList[1] === 'delete') {
-                return display.textContent = display.textContent.slice(0,-1)
-            }
-
-            if(e.target.classList[1] === 'clear'){
-                display.textContent = '';
-                value1 = '';
-                value2 = '';
-                runningResult = '';
-                return;
-            } 
-            
-            if(e.target.classList[1] === 'equal' && display.textContent === '') {
-                return;
-            } 
-
-            if (value1 === '') {
-                value1 = +display.textContent;
-                return currentOperation = e.target.classList[1];
-            }   
-
-            value2 = +display.textContent;
-            if(currentOperation === 'add') {
-                runningResult = value1 + value2;
-            } else if(currentOperation === 'subtract') {
-                runningResult = value1 - value2;
-            } else if(currentOperation === 'multiply') {
-                runningResult = value1 * value2;
-            } else if(currentOperation === 'divide') {
-
-                runningResult = value1/value2;
-            }
-            currentOperation = e.target.classList[1];
-            value1 = runningResult;
-            value2 = '';
-            display.textContent = Math.round((runningResult + Number.EPSILON)*100)/100;
-            }
-        )
-            
-});
-};
-
-operate();
-returnKeyboardCurrentOperation();
-returnKeyboardNumberInput();
+  function add(a, b) {
+    return a + b
+  }
+  
+  function subtract(a, b) {
+    return a - b
+  }
+  
+  function multiply(a, b) {
+    return a * b
+  }
+  
+  function divide(a, b) {
+    return a / b
+  }
+  
+  function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+      case '+':
+        return add(a, b)
+      case '−':
+        return subtract(a, b)
+      case '×':
+        return multiply(a, b)
+      case '÷':
+        if (b === 0) return null
+        else return divide(a, b)
+      default:
+        return null
+    }
+  }
